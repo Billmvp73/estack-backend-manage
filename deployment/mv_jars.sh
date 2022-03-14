fe829807aa411c4d7fc7b9334fbaffd829ae7ac4
pyhuang <pyhuang@umich.edu>
2022-03-13 20:30:01 -0400

    move compiled jar file to the publish folder

---
+++ deployment/mv_jars.sh
@@ -0,0 +1,4 @@
+set -x
+cp ../estack-backend-manage-api-server/target/estack-backend-manage-api-server-1.0-SNAPSHOT.jar /root/publish/estack/
+cp ../estack-backend-manage-dubbo-server/dubbo-server-user-manage/dubbo-server-user-manage-service/target/dubbo-server-user-manage-service-1.0-SNAPSHOT.jar /root/publish/estack/
+cp ../estack-backend-manage-dubbo-server/dubbo-server-house-resources/dubbo-server-house-resources-service/target/dubbo-server-house-resources-service-1.0-SNAPSHOT.jar /root/publish/estack/
